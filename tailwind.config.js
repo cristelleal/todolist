d6aa4bfdc6881c31a4f32587453fe50281ce00d8
cristelleal <cristelleal@gmail.com>
2023-12-14 12:59:06 +0100

    feat : dependencies installed successfully

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './script/main.js',
+    './style/style.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
