eace00813725cda5d9f04d7f419ff0822ddcba4c
cristelleal <cristelleal@gmail.com>
2024-01-20 16:55:21 +0100

    fix UI style pannel

--- script/main.js
+++ script/main.js
@@ -1,11 +1,15 @@
 import createTask from '../components/createTask';
 import manageTaskToUI from '../components/manageTaskToUI';
 import Client from '../class/Client';
+import hidePanel from '../components/hidePannel';
 
 async function displayTasks() {
   const tasks = await Client.getTasks();
   tasks.forEach((task) => {
     manageTaskToUI(task.title, task.id, task.completed);
+    if (task !== 0) {
+      hidePanel();
+    }
   });
 }
 
